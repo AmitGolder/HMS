12ddd674c9b2dfa32fdb80c5fa6fd39315e5f29b
Amit <info.golderamimt00@gmail.com>
2022-04-11 21:10:33 +0530

    Commit By Amit

---
+++ HospitalManagementSystem/Models/HealthDepartment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Models
+{
+    public class HealthDepartment
+    {
+        [Key]
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+    }
+}
